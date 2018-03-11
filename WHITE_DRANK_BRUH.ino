d184140880973e74eb60715ab3ff586a18b0ae51
1nsertNameHere <1nsertNameHere@users.noreply.github.com>
2018-03-11 03:02:40 -0700

    fix nic's commit

--- WHITE_DRANK_BRUH.ino
+++ WHITE_DRANK_BRUH.ino
@@ -1,5 +1,8 @@
 
 float voltage = 0;
+float voltage1 = 0;
+float voltage2 = 0;
+float voltage3 = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,11 +17,17 @@ Serial.println("Start");
 
 void loop() {
  voltage = analogRead(A0) * (5.0/1023);
- voltage1 = analogRead(A1)
- voltage2 = analogRead(A2)  
- voltage3 = analogRead(A3) 
+ voltage1 = analogRead(A1) * (5.0/1023);
+ voltage2 = analogRead(A2) * (5.0/1023);
+ voltage3 = analogRead(A3) * (5.0/1023); 
 
-   Serial.println(voltage,voltage1,voltage2,voltage3)
+   Serial.print(voltage);
+   Serial.print(" ");
+   Serial.print(voltage1);
+   Serial.print(" ");
+   Serial.print(voltage2);
+   Serial.print(" ");
+   Serial.println(voltage3);
   // put your main code here, to run repeatedly:
   delay(1000);
 }
