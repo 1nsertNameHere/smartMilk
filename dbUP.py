063b63fbaf86e33554b467054694924e27ef58dd
1nsertNameHere <1nsertNameHere@users.noreply.github.com>
2018-03-12 15:27:22 -0700

    Upload Raspberry Pi Code

---
+++ dbUP.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+from pprint import pprint
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+client = MongoClient(<<mongo URI>>")
+db = client.milkData
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
+
+dataIN = "5.00"
+milkBottle = { 'data' : dataIN } 
+result = db.milkBottles.insert_one(milkBottle)
+pprint(result)
+
+while 1:
+ dataIN = ser.readline()
+ result = db.milkBottles.update_one({}, {"$set": {"data":dataIN}} )
+ pprint(result)
